6b5bb78e7bcfe2d354cf086afdd3832c5c2977fa
Samadhan2025 <samadhan.birajdar25@gmail.com>
2026-03-04 10:54:58 +0530

    Added Button component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
+import Button from "./Button";
+
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Button Click Counter</h1>
+        <Button />
     </div>
   )
 }
